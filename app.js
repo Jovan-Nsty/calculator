c5be21ba0dfb92ef74a5a85b2c5dbb29f8d11dd7
Jovan-Nsty <jova.zyo@gmail.com>
2022-12-14 12:35:13 +0100

    Basic math operations possible

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const display = document.getElementById('display')
 const btnNumber = document.querySelectorAll('.number')
+const btnOperator = document.querySelectorAll('.operator')
+const btnEquals = document.getElementById('equals')
+
 let numberStorage = ''
+let operatorSymbol = ''
+let operandOne = 0
+let operandTwo = 0
+let result = 0
 
 btnNumber.forEach((button) => {
     button.addEventListener('click', () => {
         numberStorage += button.innerText
         displayOutput(numberStorage)
     })
+})
+
+btnOperator.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        operatorSymbol = operator.innerText
+        operandOne = parseInt(numberStorage)
+        numberStorage = ''
+    })
+})
+
+btnEquals.addEventListener('click', () => {
+    operandTwo = parseInt(numberStorage);
+    result = operate(operatorSymbol, operandOne, operandTwo)
+    displayOutput(result)
 })
 
 function displayOutput(string) {
