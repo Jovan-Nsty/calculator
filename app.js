4012ca194e1659172a7046db9888f040cf5669b3
Jovan-Nsty <jova.zyo@gmail.com>
2023-03-15 18:55:51 +0100

    Added missing semicolons

--- app.js
+++ app.js
@@ -1,153 +1,153 @@
-const display = document.getElementById('display')
-const btnNumber = document.querySelectorAll('.number')
-const btnOperator = document.querySelectorAll('.operator')
-const btnEquals = document.getElementById('equals')
-const btnClear = document.getElementById('clear')
-const btnPosNeg = document.getElementById('pos-neg')
-const btnDecimal = document.getElementById('decimal')
+const display = document.getElementById('display');
+const btnNumber = document.querySelectorAll('.number');
+const btnOperator = document.querySelectorAll('.operator');
+const btnEquals = document.getElementById('equals');
+const btnClear = document.getElementById('clear');
+const btnPosNeg = document.getElementById('pos-neg');
+const btnDecimal = document.getElementById('decimal');
 
-let numberStorage = ''
-let operatorSymbol = ''
-let operatorCount = 0
-let operandOne = 0
-let operandTwo = 0
-let result = 0
-let positiveNegativeCounter = 0
-let decimalCounter = 0
+let numberStorage = '';
+let operatorSymbol = '';
+let operatorCount = 0;
+let operandOne = 0;
+let operandTwo = 0;
+let result = 0;
+let positiveNegativeCounter = 0;
+let decimalCounter = 0;
 
 // convert number to decimal
 btnDecimal.addEventListener('click', () => {
     if (decimalCounter < 1) {
-        numberStorage = numberStorage.concat('.')
-        displayOutput(numberStorage)
-        decimalCounter++
+        numberStorage = numberStorage.concat('.');
+        displayOutput(numberStorage);
+        decimalCounter++;
     }
 })
 
 // convert number to positive or negative
 btnPosNeg.addEventListener('click', () => {
-    positiveNegativeCounter++
+    positiveNegativeCounter++;
     if (positiveNegativeCounter % 2 !== 0) {
-        numberStorage = '-'.concat(numberStorage)
-        displayOutput(numberStorage)
+        numberStorage = '-'.concat(numberStorage);
+        displayOutput(numberStorage);
     } else {
-        numberStorage = numberStorage.slice(1)
-        displayOutput(numberStorage)
+        numberStorage = numberStorage.slice(1);
+        displayOutput(numberStorage);
     }
 })
 
 // track pressed numbers
 btnNumber.forEach((button) => {
     button.addEventListener('click', () => {
-        positiveNegativeCounter = 0
-        numberStorage += button.innerText
-        displayOutput(numberStorage)
+        positiveNegativeCounter = 0;
+        numberStorage += button.innerText;
+        displayOutput(numberStorage);
     })
 })
 
 // define operator and set first operand
 btnOperator.forEach((operator) => {
     operator.addEventListener('click', () => {
-        operatorCount++
-        decimalCounter = 0
+        operatorCount++;
+        decimalCounter = 0;
         if (operatorCount > 1) {
-            pairEvaluation(operator)
+            pairEvaluation(operator);
         } else {
-            setOperator(operator.innerText)
-            setOperandOne(numberStorage)
-            numberStorage = ''
+            setOperator(operator.innerText);
+            setOperandOne(numberStorage);
+            numberStorage = '';
         }
     })
 })
 
 // define second operand and do the calculation
 btnEquals.addEventListener('click', () => {
-    setOperandTwo(numberStorage)
-    decimalCounter = 0
-    result = operate(operatorSymbol, operandOne, operandTwo)
-    checkUndefined(result)
+    setOperandTwo(numberStorage);
+    decimalCounter = 0;
+    result = operate(operatorSymbol, operandOne, operandTwo);
+    checkUndefined(result);
 })
 
 function checkUndefined(value) {
     if (value === undefined) {
-        return 0
+        return 0;
     } else {
-        displayOutput(value)
+        displayOutput(value);
     }
 }
 
 // clear display
-btnClear.addEventListener('click', () => clear())
+btnClear.addEventListener('click', () => clear());
 
 function clear() {
-    setOperandOne(0)
-    setOperandTwo(0)
-    setOperator('')
-    displayOutput(0)
-    operatorCount = 0
-    decimalCounter = 0
-    numberStorage = ''
+    setOperandOne(0);
+    setOperandTwo(0);
+    setOperator('');
+    displayOutput(0);
+    operatorCount = 0;
+    decimalCounter = 0;
+    numberStorage = '';
 }
 
 // string together several operations
 function pairEvaluation(operation) {
-    let temp = numberStorage
+    let temp = numberStorage;
     if (temp.length <= 0) {
-        return
+        return;
     } else {
-        temp = parseInt(numberStorage)
-        let result = operate(operatorSymbol, operandOne, temp)
-        setOperator(operation.innerText)
-        setOperandOne(result)
-        displayOutput(result)
-        numberStorage = ''
+        temp = parseInt(numberStorage);
+        let result = operate(operatorSymbol, operandOne, temp);
+        setOperator(operation.innerText);
+        setOperandOne(result);
+        displayOutput(result);
+        numberStorage = '';
     }
 }
 
 // set values
 function setOperandOne(value) {
     if (numberStorage === '') {
-        operandOne = 0
+        operandOne = 0;
     } else {
-        operandOne = parseFloat(value)
+        operandOne = parseFloat(value);
     }
 }
 
 function setOperandTwo(value) {
     if (numberStorage === '') {
-        operandTwo = 0
+        operandTwo = 0;
     } else {
-        operandTwo = parseFloat(value)
+        operandTwo = parseFloat(value);
     }
 }
 
 function setOperator(value) {
-    operatorSymbol = value
+    operatorSymbol = value;
 }
 
 // display content
 function displayOutput(string) {
-    display.innerText = string
+    display.innerText = string;
 }
 
 // basic math functions
 function add(a, b) {
-    return a + b
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 }
 
 function divide(a, b) {
     if (b === 0) {
-        return 'Division with 0 not possible'
+        return 'Division with 0 not possible';
     } else {
-        return a / b
+        return a / b;
     }
 }
 
@@ -155,12 +155,12 @@ function divide(a, b) {
 function operate(operator, operandOne, operandTwo) {
     switch (operator) {
         case '+':
-            return add(operandOne, operandTwo)
+            return add(operandOne, operandTwo);
         case '-':
-            return subtract(operandOne, operandTwo)
+            return subtract(operandOne, operandTwo);
         case '*':
-            return multiply(operandOne, operandTwo)
+            return multiply(operandOne, operandTwo);
         case '/':
-            return divide(operandOne, operandTwo)
+            return divide(operandOne, operandTwo);
     }
 }
